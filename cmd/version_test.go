5eaf21af71fbc6e7bd31b7d50bfafcec9939792e
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-06-01 17:33:40 -0600

    Add some tests

---
+++ cmd/version_test.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"bytes"
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func TestUndefinedVersion(t *testing.T) {
+	b := bytes.NewBuffer(nil)
+	SetVersion("")
+	version := &versionCmd{
+		out: b,
+	}
+	err := version.run()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "kubectl server-version \n", b.String())
+}
+
+func TestSemanticVersion(t *testing.T) {
+	b := bytes.NewBuffer(nil)
+	SetVersion("1.2.3")
+	version := &versionCmd{
+		out: b,
+	}
+	err := version.run()
+
+	assert.Nil(t, err)
+	assert.Equal(t, "kubectl server-version 1.2.3\n", b.String())
+}
+
+func TestVersionForError(t *testing.T) {
+	writerMock := new(WriterMock)
+	SetVersion("1.2.3")
+	version := &versionCmd{
+		out: writerMock,
+	}
+	writerMock.On("Write", []byte("kubectl server-version 1.2.3\n")).Return(0, errors.New("expected"))
+	err := version.run()
+
+	writerMock.AssertExpectations(t)
+	assert.NotNil(t, err)
+	assert.Equal(t, "expected", err.Error())
+}
+
+type WriterMock struct {
+	mock.Mock
+}
+
+func (w *WriterMock) Write(p []byte) (n int, err error) {
+	args := w.Called(p)
+	return args.Int(0), args.Error(1)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/spf13/cobra v0.0.4
+	github.com/stretchr/testify v1.3.0
 	k8s.io/api v0.0.0-20190531132110-47f80a4c388d // indirect
 	k8s.io/apimachinery v0.0.0-20190531095030-97c9a3e58b4c // indirect
 	k8s.io/cli-runtime v0.0.0-20190528160946-15ce19e2add9
